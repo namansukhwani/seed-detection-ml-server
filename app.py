59bfa36171f13bb5fe167fd1c3edeadfd79d66d1
Naman Sukhwani <namansukhwani@gmail.com>
2022-01-13 15:05:06 +0530

    update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# import numpy as np
-# import cv2
-# import requests as fetch
-# from PIL import Image
-# from io import BytesIO
-# from keras.models import load_model
-# model = load_model('multiclass_model80_77.h5')
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+import numpy as np
+import cv2
+import requests as fetch
+from PIL import Image
+from io import BytesIO
+from keras.models import load_model
+model = load_model('multiclass_model80_77.h5')
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #flask imports
 from flask import Flask
@@ -30,23 +30,23 @@ api=Api(app)
 
 # #Flask responce class        
             
-# def urlToImage(url):
-#     with open('tempImage.jpg', 'wb') as f:
-#         f.write(fetch.get(url).content)
-#     image=Image.open('tempImage.jpg')
-#     new_image=image.resize((128,128))
-#     new_image.save('finalImg.jpg')
-#     return 'finalImg.jpg'
+def urlToImage(url):
+    with open('tempImage.jpg', 'wb') as f:
+        f.write(fetch.get(url).content)
+    image=Image.open('tempImage.jpg')
+    new_image=image.resize((128,128))
+    new_image.save('finalImg.jpg')
+    return 'finalImg.jpg'
     
 
-# def pred(url):
-#     # image=Image.open('img2.jpg')
-#     # new_image=image.resize((128,128))
-#     # new_image.save('finalImg.jpg')
-#     k = np.expand_dims(cv2.imread(urlToImage(url))*(1.0/255.0), axis=0)
-#     p = model.predict(k)
-#     a = np.argmax(p)
-#     return {"totalQuality":a,"data":p}
+def pred(url):
+    image=Image.open('img2.jpg')
+    new_image=image.resize((128,128))
+    new_image.save('finalImg.jpg')
+    k = np.expand_dims(cv2.imread(urlToImage(url))*(1.0/255.0), axis=0)
+    p = model.predict(k)
+    a = np.argmax(p)
+    return {"totalQuality":a,"data":p}
 
 class SeedQuality(Resource):
     def post(self):
@@ -56,12 +56,12 @@ class SeedQuality(Resource):
         
         url=req_json['url']
         
-        # result=pred(url)
+        result=pred(url)
 
         return{
             "status":True,
             "message":"responce sucessful",
-            "data":"2",
+            "data":result,
             "imageUrl":url
         },200
 
